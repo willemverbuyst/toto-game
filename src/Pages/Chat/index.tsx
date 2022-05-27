ee04bfffc78b3b78e1caab5bc5d5ec6a66592cea
Willem Verbuyst <willemverbuyst@gmail.com>
2022-05-27 05:32:00 +0200

    Add socketio to client

--- src/Pages/Chat/index.tsx
+++ src/Pages/Chat/index.tsx
@@ -1,21 +1,27 @@
 import { Box } from '@material-ui/core'
-import React, { ReactElement } from 'react'
-
-// import { useHistory } from 'react-router-dom'
-
+import React, { useState, useEffect, ReactElement } from 'react'
+import socketIOClient from 'socket.io-client'
 import PageTitle from '../../Components/Title/PageTitle'
 
-const Chat: React.FC = (): ReactElement => {
-  // const history = useHistory()
+const ENDPOINT = 'http://127.0.0.1:9000'
 
-  // useEffect(() => {
-  //   if (token) history.push('/programma')
-  // }, [token, history])
-  // }
+const Chat: React.FC = (): ReactElement => {
+  const [response, setResponse] = useState('')
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT)
+    socket.on('messageFromServer', (data) => {
+      setResponse(data.message)
+    })
+    socket.emit('messageFromClient', {
+      message: 'This is a message from the client',
+    })
+  }, [])
 
   return (
     <Box>
       <PageTitle title="Chat" color="secondary" />
+      <p>Message: {response}</p>
     </Box>
   )
 }
